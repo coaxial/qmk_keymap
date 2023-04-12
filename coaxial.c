d8f72d774ce1a34a0a2fb3ca94b787940f2fbe49
coaxial <c@64b.it>
2023-04-12 10:43:11 +0200

    Rotate display on right hand

--- coaxial.c
+++ coaxial.c
@@ -10,6 +10,14 @@ enum layer_number {
 };
 
 # ifdef OLED_ENABLE
+oled_rotation_t oled_init_user(oled_rotation_t rotation) {
+    if (is_keyboard_master()) {
+        return OLED_ROTATION_0;
+    } else {
+        return OLED_ROTATION_180;
+    }
+}
+
 // Customize layer names
 static void render_layer_status(void) {
     char buf[10];
