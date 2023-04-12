7136243ee5daf6a90f6c095db98849038bed60b7
Rock <coaxial@users.noreply.github.com>
2023-04-12 10:19:08 +0200

    feat: show actual layer names (#4)
    * Attempt to override layer names and count
    
    * Rename custom code file
    
    * Add missing includes
    
    * Try with a header file
    
    * Add missing function signature
    
    * Include default/oled_display.c
    
    * Improve portability across keyboards
    
    * Add missing functions
    
    * Reorder functions

--- rules.mk
+++ rules.mk
@@ -1,3 +1,10 @@
-SRC += source.c
+SRC += coaxial.c
 
-EXTRAKEY_ENABLE = yes # Enable Audio and System control keys
+EXTRAKEY_ENABLE = yes     # Enable Audio and System control keys
+HELIX_ROWS = 5
+LED_ANIMATIONS = no       # No LEDs on my keyboard
+LED_BACK_ENABLE = no      # No LEDs on my keyboard
+LED_UNDERGLOW_ENABLE = no # No LEDs on my keyboard
+MOUSEKEY_ENABLE = yes     # For the Mouse layer
+OLED_ENABLE = yes
+OLED_SELECT = core        # Use the standard driver for OLED
