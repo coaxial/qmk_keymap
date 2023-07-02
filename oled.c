8dbac1e280ddbe4cfd53ecb57ddef9b2fbf22499
coaxial <c@64b.it>
2023-07-02 15:03:19 +0200

    refactor: Fix indent

--- oled.c
+++ oled.c
@@ -49,34 +49,34 @@ static void render_layer_status(void) {
 
 void render_status(void) {
   // Render to mode icon
-    static const char os_logo[][2][3] PROGMEM = {
-      {
-        {0x95,0x96,0}, {0xb5,0xb6,0},
-      },
-      {
-        {0x97,0x98,0}, {0xb7,0xb8,0},
-      }
-    };
-    if (is_mac_mode()) {
-        oled_write_P(os_logo[0][0], false);
-        oled_write_P(PSTR("\n"), false);
-        oled_write_P(os_logo[0][1], false);
-    } else {
-        oled_write_P(os_logo[1][0], false);
-        oled_write_P(PSTR("\n"), false);
-        oled_write_P(os_logo[1][1], false);
+  static const char os_logo[][2][3] PROGMEM = {
+    {
+      {0x95,0x96,0}, {0xb5,0xb6,0},
+    },
+    {
+      {0x97,0x98,0}, {0xb7,0xb8,0},
     }
-
-    oled_write_P(PSTR(" "), false);
-    render_layer_status();
-
-    // Host Keyboard LED Status
-    led_t led_state = host_keyboard_led_state();
-    oled_write_P(led_state.num_lock ? PSTR("NUMLOCK") : PSTR("       "), false);
-    oled_write_P(led_state.caps_lock ? PSTR("CAPS") : PSTR("    "), false);
-    oled_write_P(led_state.scroll_lock ? PSTR("SCLK") : PSTR("    "), false);
-    oled_advance_page(true);
+  };
+  if (is_mac_mode()) {
+    oled_write_P(os_logo[0][0], false);
     oled_write_P(PSTR("\n"), false);
+    oled_write_P(os_logo[0][1], false);
+  } else {
+    oled_write_P(os_logo[1][0], false);
+    oled_write_P(PSTR("\n"), false);
+    oled_write_P(os_logo[1][1], false);
+  }
+
+  oled_write_P(PSTR(" "), false);
+  render_layer_status();
+
+  // Host Keyboard LED Status
+  led_t led_state = host_keyboard_led_state();
+  oled_write_P(led_state.num_lock ? PSTR("NUMLOCK") : PSTR("       "), false);
+  oled_write_P(led_state.caps_lock ? PSTR("CAPS") : PSTR("    "), false);
+  oled_write_P(led_state.scroll_lock ? PSTR("SCLK") : PSTR("    "), false);
+  oled_advance_page(true);
+  oled_write_P(PSTR("\n"), false);
 }
 
 bool oled_task_user(void) {
